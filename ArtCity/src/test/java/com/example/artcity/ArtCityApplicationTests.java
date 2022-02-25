cc7e0e5b97065463d41d5c5e3de8a740aeac4ecb
Wiji <peawseaw@gmail.com>
2022-02-24 21:04:19 -0800

    create project

---
+++ ArtCity/src/test/java/com/example/artcity/ArtCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
